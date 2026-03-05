2dbd4d8e0b7784949e49218c44473174cbf44720
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2026-03-05 01:08:34 +0000

    python

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+print("hola mundo cruel!!!!")
+
+# VARIABLES
+
+nombre = "cristian"
+edad = 19
+altura = 2.00
+es_profesor = True
+ciudad = "valparaiso"
+
+
+#TIPOS DE DATOS
+
+str #esto es un texto
+int #esto es un entero
+float #esto es un flotante 
+bool #esto es verdadero  o falso
+
+#INPUT
+#input()
+
+
+texto = input()
+print(texto)
+
+
+
